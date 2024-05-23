1512e517651afebcd4bd66ba9f22c79c5c9c2e78
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-05-23 21:48:08 +0100

    tried new method to report key

---
+++ request.js
@@ -0,0 +1,6 @@
+export default async (request, context) => {
+    const value = Netlify.env.get("MAP_API");
+    return new Response(`Value of MY_IMPORTANT_VARIABLE for ${context.site.name} is ${value}.`, {
+      headers: { "content-type": "text/html" },
+    });
+  };
