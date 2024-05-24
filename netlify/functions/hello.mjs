b9466ab5c6a32f2f53c25ec4c00f9a59f0a3c0d5
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-05-24 21:12:43 +0100

    testing

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,3 +1,13 @@
+/*
 export default async (req, context) => {
     return new Response("Hello, world!");
   };
+  */
+
+  exports.handler = async event => {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+        statusCode: 200,
+        body: `Hello ${subject}!`,
+    }
+}
