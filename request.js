0fead7d2ac9a2333fd1b80257e086e33f948dbbf
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-05-27 00:03:13 +0100

    testing

--- request.js
+++
@@ -1,6 +0,0 @@
-export default async (request, context) => {
-    const value = Netlify.env.get("MAP");
-    return new Response(`Value of MY_IMPORTANT_VARIABLE for ${context.site.name} is ${value}.`, {
-      headers: { "content-type": "text/html" },
-    });
-  };
