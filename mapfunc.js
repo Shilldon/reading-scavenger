7c354022e2b2c686b92c850defde5d13d3e64361
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-05-27 21:45:21 +0100

    testing

--- mapfunc.js
+++ mapfunc.js
@@ -1,6 +1,7 @@
 function loadMap(key) {
-    console.log("running load map?")
-    g => {
+    (g => {
+        console.log("running load map?");
+
         var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;
         b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{
             await (a=m.createElement("script"));
@@ -10,7 +11,7 @@ function loadMap(key) {
             d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));
             a.nonce=m.querySelector("script[nonce]")?.nonce||"";
             m.head.append(a)}));
-            d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))}
+            d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
             ({
                 key: `"${key}"`,
                 v: "weekly",
