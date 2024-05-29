cc5aa08524d87932abaec97f77114f16b9198296
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-05-29 22:40:18 +0100

    added center on user button

--- mapfunc.js
+++ mapfunc.js
@@ -1,24 +1,26 @@
 
 //function to submit api key and define map
 function loadMap(key) {
-    (g => {
-        var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;
-        b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{
-            await (a=m.createElement("script"));
-            e.set("libraries",[...r]+"");
-            for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);
-            e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;
-            d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));
-            a.nonce=m.querySelector("script[nonce]")?.nonce||"";
-            m.head.append(a)}));
-            d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-            ({
-                key: `${key}`,
-                v: "weekly",
-                // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-                // Add other bootstrap parameters as needed, using camel case.
-            })
-    initMap();
+  (g => {
+    var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window;
+    b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => {
+      await (a = m.createElement("script"));
+      e.set("libraries", [...r] + "");
+      for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+      e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+      d[q] = f; a.onerror = () => h = n(Error(p + " could not load."));
+      a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+      m.head.append(a)
+    }));
+    d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+  })
+    ({
+      key: `${key}`,
+      v: "weekly",
+      // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+      // Add other bootstrap parameters as needed, using camel case.
+    })
+  initMap();
 }
 
 //set up map markers
@@ -31,9 +33,9 @@ let map;
 let marker;
 //set up map for first time
 async function initMap() {
-  
+
   const { Map } = await google.maps.importLibrary("maps");
-  const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
   //set initial position
   map = new Map(document.getElementById("map"), {
@@ -42,16 +44,14 @@ async function initMap() {
     mapId: "32becf6749a12dee"
   });
 
-          //set marker
-          marker = new AdvancedMarkerElement({
-            title: 'Your Location',
-            content: currentLocImg,
-            map: map
-          });
-  
-          updateLocation();  
-  
-          
+  //set marker
+  marker = new AdvancedMarkerElement({
+    title: 'Your Location',
+    content: currentLocImg,
+    map: map
+  });
+
+
   const locationButton = document.createElement("button");
   locationButton.textContent = "Pan to Current Location";
   locationButton.classList.add("custom-map-control-button");
@@ -59,18 +59,24 @@ async function initMap() {
   locationButton.addEventListener("click", () => {
     centreOnUser();
   })
+
+  //start updating user location
+  updateLocation();
+
+
+
   //handle user location
-//  infoWindow = new google.maps.InfoWindow();
-/*
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-
-  locationButton.addEventListener("click", () => {*/
-    // Try HTML5 geolocation.
-//  });
+  //  infoWindow = new google.maps.InfoWindow();
+  /*
+    const locationButton = document.createElement("button");
+  
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  
+    locationButton.addEventListener("click", () => {*/
+  // Try HTML5 geolocation.
+  //  });
 }
 
 
@@ -94,9 +100,9 @@ function updateLocation() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-  setTimeout(function() {
+  setTimeout(function () {
     updateLocation();
-  },1000)
+  }, 1000)
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -128,7 +134,7 @@ function centreOnUser() {
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
-  }  
+  }
 }
 window.initMap = initMap;
 
