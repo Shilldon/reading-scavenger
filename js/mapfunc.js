444ebeeb9637055c73b16f9d9f9775ea0e287ffc
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-01 22:23:36 +0100

    clue distance checker on new GeoMarker

--- js/mapfunc.js
+++ js/mapfunc.js
@@ -274,8 +274,8 @@ function toRad(Value) {
 
 function getDistanceBetween(lat1, lon1) {
   console.log(GeoMarker)
-  var lat2 = GeoMarker.get("postion").lat();
-  var lon2 = GeoMarker.get("postion").lng();
+  var lat2 = GeoMarker.get("position").lat();
+  var lon2 = GeoMarker.get("position").lng();
 
 console.log(`lat1 ${lat1} lat2 ${lat2} lon1 ${lon1} lon2 ${lon2}`)
 
