6f7847fb95e05c99429c30ce7ce9e7b9ef0159a9
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-07 00:05:52 +0100

    added database

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -15,7 +15,7 @@ export default async () => {
     const { data, error } = await supabase
         .from('clues')
         .insert([
-            { note: 'THis is a clue' },
+            { status: 'THis is a clue' },
         ]);
 
   // Did it work?
