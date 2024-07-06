7922c731f0504ab7ed8afb6e6791326569a0e24d
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-07 00:01:46 +0100

    added database

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const {
     SITE,
     SUPABASE
-} = process.env;
+} = process.env;*/
 
 // Connect to our database 
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(SITE, SUPABASE);
+const supabase = createClient('https://rqkdrjczypzxuyzyrins.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxa2RyamN6eXB6eHV5enlyaW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAzMDA3NDAsImV4cCI6MjAzNTg3Njc0MH0.yzsaK3btoSwnYlVxxWiGSw0FDzt0UMdRzORtAzCWp5Y');
 
 // Our standard serverless handler function
 //exports.handler = async event => {
