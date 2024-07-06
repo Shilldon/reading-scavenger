b0ceb94c4f33d1e696f184604a08615c853fc049
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-06 22:55:15 +0100

    attempt to fix map style not being applied

--- netlify/functions/request.ts
+++ netlify/functions/request.ts
@@ -1,7 +1,13 @@
+import type { Context, Config } from "@netlify/edge-functions";
+
 export default async (req, context) => {
-  return new Response.json({
+  return Response.json({
     "map":process.env.MAP,
     "supabase": process.env.SUPABASE,
     "site":process.env.SUPABASE_SITE
   });
+};
+
+export const config: Config = {
+  path: "/json",
 };
