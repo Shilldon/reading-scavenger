e36c5cda177c6d54611558bdf11d96626cb3d5b5
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-07 01:07:14 +0100

    added database

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -12,17 +12,16 @@ const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
 // Our standard serverless handler function
 //exports.handler = async event => {
-    exports.handler = async (event, context) => {
-        try {
-          const { data, error } = await supabase.from('clues').select('*');
-          return {
-            statusCode: 200,
-            body: JSON.stringify(data),
-          };
-        } catch (err) {
-          return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Internal server error' }),
-          };
-        }
-      };
+    exports.handler = async event => {
+
+        // Insert a row
+          const { data, error } = await supabase
+              .from('clues')
+              .insert([
+                  { id: 2, status: 'I need to not forget this' },
+              ]);
+      
+        // Did it work?
+        console.log(data, error);
+        
+      }
