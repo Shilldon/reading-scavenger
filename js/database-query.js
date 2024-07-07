50c23f200bfb133448e30cd56c2a7bdb32789497
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-07 10:26:23 +0100

    added database

--- js/database-query.js
+++ js/database-query.js
@@ -1,6 +1,13 @@
-function databaseQuery(keys) {
+async function databaseQuery(keys) {
     const url = keys.site;
     const key = keys.supabase;
     const database = supabase.createClient(url,key);
     console.log(database);
+
+    const res = await database.from("clues").insert({
+        id: 2,
+        clue: "This is another clue"
+    })
+
+    console.log(res);
 }
