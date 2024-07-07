e696237f5b131491660349292dc6db7262087cf6
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-07 01:10:43 +0100

    added database

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -5,8 +5,8 @@ const {
 } = process.env;*/
 
 // Connect to our database 
-const DATABASE_URL = process.env.get("SUPABASE_SITE");
-const SUPABASE_SERVICE_API_KEY = process.env.get("SUPABASE");
+const DATABASE_URL = Netlify.env.get("SUPABASE_SITE");
+const SUPABASE_SERVICE_API_KEY = Netlify.env.get("SUPABASE");
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
