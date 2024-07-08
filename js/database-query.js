e213a01289ba388213d00c504165619bfb81dc9b
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-08 18:49:33 +0100

    added scoring system

--- js/database-query.js
+++ js/database-query.js
@@ -1,18 +1,4 @@
-async function databaseQuery(keys) {
-    const url = keys.site;
-    const key = keys.supabase;
-    const database = supabase.createClient(url,key);
-    console.log(database);
-
-    const res = await database.from("clues").insert({
-        id: 2,
-        status: "This is another clue"
-    })
-
-    console.log(res);
-}
-
-async function captureMarker(keys, marker, team, clueMarkers) {
+async function captureMarker(keys, marker, team, clueMarkers,score) {
     const url = keys.site;
     const key = keys.supabase;
     const database = supabase.createClient(url,key);
@@ -22,6 +8,22 @@ async function captureMarker(keys, marker, team, clueMarkers) {
         .from("clues")
         .update({status: team})
         .eq("id",marker);
+
+    let points = clueMarkers[marker].points;
+
+    const { data, error } = await database
+    .from('positions')
+    .select('score')
+    .eq('name',team);
+
+    let score = data[0].score;
+    score = score + points;
+
+    const updatedScore = await database
+    .from("positions")
+    .update({"team": team})
+    .eq("score",score);
+
 
     clueMarkers[marker-1].content.setAttribute("captured",team);
     clueMarkers[marker-1].content.src = "./icons/captured-"+team+".png";
@@ -39,7 +41,7 @@ async function checkStatus(keys,marker,clueMarkers) {
         .from('clues')
         .select('status')
         .eq('id',marker)
-        console.log(data)
+
     if(data[0].status == "active") {
         let team = document.body.getAttribute("data-team");
         console.log("team is "+team)
@@ -57,6 +59,7 @@ async function checkStatus(keys,marker,clueMarkers) {
         failCapture(marker, captureText,data[0].status);
     }
 }
+
 async function getCapturedStatus(keys) {
     const url = keys.site;
     const key = keys.supabase;
@@ -65,7 +68,6 @@ async function getCapturedStatus(keys) {
     
     let capturedArray = {};
     for(i=0;i<res.data.length;i++) {
-    //  console.log("captured - "+res.data[i].id+" is "+res.data[i].status)
       capturedArray[res.data[i].id] = res.data[i].status;
     }
 
--- js/mapfunc.js
+++ js/mapfunc.js
@@ -83,250 +83,277 @@ function fullScreenToggle() {
 }
 
 
-//set up map markers
-/*
-const currentLocImg = document.createElement("img");
-currentLocImg.src = "./icons/location.gif";
-currentLocImg.className = "marker-img";
-*/
 
 //define the map variable
 let map;
 let GeoMarker;
-//let userMarker;
 let infoWindow;
 let clueMarker;
+
 let clues = {
   "1": {
     "lat": 51.455050,
     "lng": -0.960765,
     "clue": "When did we first produce biscuits?",
     "answer": 1814,
-    "captured":"false"
+    "captured":"false",
+    "points":10
   },
   "2": {
     "lat": 51.453957,
     "lng": -0.961984,
     "clue": "59 and ...",
     "answer": 75,
-    "captured":"false"
+    "captured":"false",
+    "points": 8
   },
   "3": {
     "lat": 51.453083,
     "lng": -0.969136,
     "clue": "First tram?",
     "answer": 1903,
-    "captured":"false"
+    "captured":"false",
+    "points": 7
   },
   "4": {
     "lat": 51.452515,
     "lng": -0.971589,
     "clue": "Bruce Tunnel - height above sea level?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 450,
+    "captured":"false",
+    "points": 6
   },
   "5": {
     "lat": 51.452623,
     "lng": -0.972350,
     "clue": "How many taxis stand here?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 3,
+    "captured":"false",
+    "points":6
   },
   "6": {
     "lat": 51.454097,
     "lng": -0.966383,
-    "clue": "What year deen Maureen Lockey open this?",
-    "answer": 1814,
-    "captured":"false"
+    "clue": "What year did Maureen Lockey open this?",
+    "answer": 1990,
+    "captured":"false",
+    "points":4
   },
   "7": {
     "lat": 51.454171,
     "lng": -0.967394,
     "clue": "How many birds?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 8,
+    "captured":"false",
+    "points":4
   },
   "8": {
     "lat": 51.454675,
     "lng": -0.966327,
     "clue": "What year was this completed?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 2007,
+    "captured":"false",
+    "points":3
   },
   "9": {
     "lat": 51.455797,
     "lng": -0.966093,
     "clue": "When was Abbot Hugh executed?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1539,
+    "captured":"false",
+    "points":3
   },
   "10": {
     "lat": 51.4565098732022,
     "lng": -0.976282147785361,
     "clue": "Middle of the black V (look carefully, this one's small!)?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 3,
+    "captured":"false",
+    "points":10
   },
   "11": {
     "lat": 51.456777,
     "lng": -0.974131,
     "clue": "How many bollards outside Siren?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 14,
+    "captured":"false",
+    "points":7
   },
   "12": {
     "lat": 51.454582,
     "lng": -0.977798,
     "clue": "Orange planters?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 9,
+    "captured":"false",
+    "points":8
   },
   "13": {
     "lat": 51.456053,
     "lng": -0.977058,
     "clue": "What location do you quote to 'save time'?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 9431,
+    "captured":"false",
+    "points":7
   },
   "14": {
     "lat": 51.453863,
     "lng": -0.977308,
     "clue": "How many sides?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 8,
+    "captured":"false",
+    "points":7
   },
   "15": {
     "lat": 51.45420432233849,
     "lng": -0.973830158038599,
     "clue": "How old was Peter Inman?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 76,
+    "captured":"false",
+    "points":6
   },
   "16": {
     "lat": 51.454002,
     "lng": -0.973724,
     "clue": "How many volts?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 240,
+    "captured":"false",
+    "points":7
   },
   "17": {
     "lat": 51.4548622442915,
     "lng": -0.9732989962959214,
     "clue": "When was Bill's drainpipe installed?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1931,
+    "captured":"false",
+    "points":8
   },
   "18": {
     "lat": 51.456372,
     "lng": -0.967826,
     "clue": "When was the Abbey severely damaged?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1643,
+    "captured":"false",
+    "points":1
   },
   "19": {
     "lat": 51.456969094260906,
     "lng": -0.9674520989827075,
     "clue": "How many captains?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 4,
+    "captured":"false",
+    "points":2
   },
   "20": {
     "lat": 51.45695789637502,
     "lng": -0.9682384625131224,
     "clue": "Romans were writing on the wall.",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 20,
+    "captured":"false",
+    "points":3
   },
   "21": {
     "lat": 51.45786784843759,
     "lng": -0.9694736110905124,
     "clue": "You cannot miss this one!",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 12,
+    "captured":"false",
+    "points":1
   },
   "22": {
     "lat": 51.45810805988329,
     "lng": -0.972069271328189,
-    "clue": "Who is this?",
-    "answer": 1814,
-    "captured":"false"
+    "clue": "Edward the ...",
+    "answer": 7,
+    "captured":"false",
+    "points":4
   },
   "23": {
     "lat": 51.45663880429062,
     "lng": -0.9678497364696548,
     "clue": "When did John Speed make his map?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1610,
+    "captured":"false",
+    "points":2
   },
   "24": {
     "lat": 51.45654731484567,
     "lng": -0.967196205486239,
     "clue": "What year was Frederick Potts in Gallipoli?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1915,
+    "captured":"false",
+    "points":1
   },
   "25": {
     "lat": 51.45502978513994,
     "lng": -0.970030106107138,
     "clue": "George knows his Roman numerals...",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1506,
+    "captured":"false",
+    "points":5
   },
   "26": {
     "lat": 51.4566599803753,
     "lng": -0.97000077524566853,
     "clue": "Right circle minus left circle = ?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 57,
+    "captured":"false",
+    "points":3
   },
   "27": {
     "lat": 51.45684525008421,
     "lng": -0.9722219340620878,
     "clue": "Anno domini",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1902,
+    "captured":"false",
+    "points":3
   },
   "28": {
     "lat": 51.456710589169155,
     "lng": -0.9723428065772316,
     "clue": "When did they first start surveying, or auctioning?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1882,
+    "captured":"false",
+    "points":4
   },
   "29": {
     "lat": 51.45669965202262,
     "lng": -0.9718587723006621,
     "clue": "666 maybe the number of the beast, what is the number of the dog?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 150,
+    "captured":"false",
+    "points":3
   },
   "30": {
     "lat": 51.45670984883732,
     "lng": -0.9719931584053527,
     "clue": "How many minutes to get to the Abbey Ruins?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 6,
+    "captured":"false",
+    "points":3
   },
   "31": {
     "lat": 51.45527591728553,
     "lng": -0.9707796488937782,
     "clue": "O, how many blue plaques above this?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 5,
+    "captured":"false",
+    "points":5
   },
   "32": {
     "lat": 51.45631791329671,
     "lng": -0.9695816750144369,
     "clue": "When was this 'clock' installed?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1727,
+    "captured":"false",
+    "points":3
   },
   "33": {
     "lat":   51.45558870304998, 
     "lng": -0.9680066913345313,
     "clue": "Question 1 - what number to unlock our door?",
-    "answer": 1814,
-    "captured":"false"
+    "answer": 1289,
+    "captured":"false",
+    "points":0
   }
 }
 
@@ -567,7 +594,6 @@ function getDistanceBetween(lat1, lon1) {
 
 
 function checkAnswer(answer,location) {
-  console.log("answer "+answer+" location "+location)
   if(answer == clues[`${location}`].answer) {
     checkStatus(keys,location,clueMarkers);
   }
