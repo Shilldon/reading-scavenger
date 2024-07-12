60c20761cc1dcff4013f785daf6a93282a35dfab
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-12 21:52:55 +0100

    tracking of teams added

--- netlify/functions/getMap.js
+++ netlify/functions/getMap.js
@@ -1,14 +1,24 @@
+// netlify/functions/getMapData.js
+const fetch = require('node-fetch');
 
-import fetch from 'node-fetch';
 exports.handler = async (event, context) => {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
   const { lat, lng } = JSON.parse(event.body);
 
-  const response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
-  const data = await response.json();
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' }),
+    };
+  }
 };
