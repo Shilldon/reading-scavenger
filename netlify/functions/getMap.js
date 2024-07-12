7fe95eb22751ff612586b333c79ff188322f13fe
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-12 14:11:17 +0100

    tracking of teams added

--- netlify/functions/getMap.js
+++ netlify/functions/getMap.js
@@ -4,11 +4,11 @@ exports.handler = async (event, context) => {
   const apiKey = process.env.MAP;
   const { lat, lng } = JSON.parse(event.body);
 
-  const response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
-  const data = await response.json();
+  //const response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
+  //const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify(apiKey),
   };
 };
