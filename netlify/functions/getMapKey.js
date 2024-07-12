624b5f87b1d8867d99510afa6b575aa1b4a214c9
Stewart Stretton-Hill <stewart.stretton-hill@ntlworld.com>
2024-07-12 11:45:12 +0100

    tracking of teams added

--- netlify/functions/getMapKey.js
+++ netlify/functions/getMapKey.js
@@ -1,8 +1,14 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event, context) => {
-    const apiKey = process.env.MAP;
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ apiKey }),
-    };
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const { lat, lng } = JSON.parse(event.body);
+
+  const response = await fetch(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
   };
+};
